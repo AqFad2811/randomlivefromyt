afb88cb5fd3fc586f857b084b3adaae4a70da103
AqFad <85995579+AqFad2811@users.noreply.github.com>
2021-09-22 10:17:14 +0800

    Update autorun5.sh

--- autorun5.sh
+++ autorun5.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 youtube_m3ugrabber5.py > ../AlQuranAlKareem.m3u8
+python3 youtube_m3ugrabber5.py > ../youtube5.m3u8
 
 echo m3u grabbed
