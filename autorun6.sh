1afe3a06a65a74476dc23040a839447111e6e69b
AqFad <85995579+AqFad2811@users.noreply.github.com>
2022-02-05 00:12:09 +0800

    Update autorun6.sh

--- autorun6.sh
+++ autorun6.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 youtube_m3ugrabber6.py > ../youtube6.m3u8
+python3 youtube_m3ugrabber6.py > ../AllTheKPOP.m3u8
 
 echo m3u grabbed
